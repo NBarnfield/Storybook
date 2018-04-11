5d89882aa52223935eddf4ede2b408504d0ac789
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-12 08:53:40 +1000

    List sorting has been sorted...Flesh out story line in options.

--- Quips and breaks/shopping.py
+++ Quips and breaks/shopping.py
@@ -3,7 +3,7 @@
 def shopping():
 
     grocery_list = []
-    grocery_items = ['slippers', 'newspaper', 'milk'] #list.sort(['slippers', 'newspaper', 'milk'])
+    grocery_items = ['milk', 'newspaper', 'slippers']
 
     while grocery_list != grocery_items:
         print("\nItems in your possession: {}".format(grocery_list))
@@ -13,7 +13,7 @@ def shopping():
         if choice == 'a' and 'slippers' not in grocery_list:
             print("")
             grocery_list.append('slippers')
-            #list.sort([grocery_list])
+            grocery_list.sort()
 
         elif choice == 'b' and 'newspaper' not in grocery_list:
             print("You walk down the sundries isle and can see ol' lady buscuit Barbarra eying off the last edition of 'The Daily Bake'.")
@@ -22,18 +22,18 @@ def shopping():
             if dive == 'y' or 'Y':
                 print("You have won, Barbarra is down for the count and you have your newspaper")
                 grocery_list.append('newspaper')
-                #grocery_list = list.sort([grocery_list])
+                grocery_list.sort()
 
             else:
                 print("Through some arcane socery you have aquired the paper...Barbarra sensed your intention..."
                       "and backflipped over horseshit to wrangle it from your claws and fell in dramaic fashion, "
                       "leaving you the clear victor. You have your newspaper.")
                 grocery_list.append('newspaper')
-                #list.sort([grocery_list])
+                grocery_list.sort()
 
         elif choice == 'c'and 'milk' not in grocery_list:
             grocery_list.append('milk')
-            #list.sort([grocery_list])
+            grocery_list.sort()
 
         else:
             print("That isn't a valid option!")
