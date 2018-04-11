270c1bb1d5fd527384abff1b3249bfb55ef5ecce
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-04-12 08:49:29 +1000

    Added not in criteria to the list function to allow for data validation of the lits. Add a sort function to allow for grocery list and grocery items to be compared correctly without the neccesity for them to be in the exact same order.

--- Quips and breaks/shopping.py
+++ Quips and breaks/shopping.py
@@ -3,35 +3,40 @@
 def shopping():
 
     grocery_list = []
-    grocery_items = ['slippers', 'newspaper', 'milk']
+    grocery_items = ['slippers', 'newspaper', 'milk'] #list.sort(['slippers', 'newspaper', 'milk'])
 
     while grocery_list != grocery_items:
-        print("Items in your possession: {}".format(grocery_list))
-        print("You are standing in the supermaket - What would you like to find?" )
+        print("\nItems in your possession: {}".format(grocery_list))
+        print("\nYou are standing in the supermaket - What would you like to find?" )
         choice = input(" a) slippers b) newspaper c) milk")
 
-        if choice == 'a' and grocery_list != 'slippers':
+        if choice == 'a' and 'slippers' not in grocery_list:
+            print("")
             grocery_list.append('slippers')
+            #list.sort([grocery_list])
 
-
-        elif choice == 'b' and grocery_list != 'newspaper':
-            print("You walk down the sundries isle and can see ol' lady buscuit Barbarra eying off the last edition of 'The Daily Bake.")
+        elif choice == 'b' and 'newspaper' not in grocery_list:
+            print("You walk down the sundries isle and can see ol' lady buscuit Barbarra eying off the last edition of 'The Daily Bake'.")
             print("There is no way you are going to lose to this honeybun.")
             dive = input("Will you dive for the paper (y/n)? ")
             if dive == 'y' or 'Y':
                 print("You have won, Barbarra is down for the count and you have your newspaper")
                 grocery_list.append('newspaper')
+                #grocery_list = list.sort([grocery_list])
+
             else:
                 print("Through some arcane socery you have aquired the paper...Barbarra sensed your intention..."
                       "and backflipped over horseshit to wrangle it from your claws and fell in dramaic fashion, "
                       "leaving you the clear victor. You have your newspaper.")
                 grocery_list.append('newspaper')
+                #list.sort([grocery_list])
 
-        elif choice == 'c'and grocery_list != 'milk':
+        elif choice == 'c'and 'milk' not in grocery_list:
             grocery_list.append('milk')
+            #list.sort([grocery_list])
+
         else:
             print("That isn't a valid option!")
-            exit()
 
 shopping()
 print("You have everything required to appease Mrs CCB and perhaps learn an insight or two into what she has been doing...")
