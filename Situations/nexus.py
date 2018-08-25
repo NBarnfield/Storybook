5ebfa0ab647a41554d47089c5b289ef36931b938
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2018-08-25 13:59:30 +1000

    New situations for insertion into stories.

---
+++ Situations/nexus.py
@@ -0,0 +1,25 @@
+# Two men at the bus centre discussing religion
+print("""The two master craftsmen sit with tea whispering with prozeltising intent. Respectfully they attempt to convince
+one another that their text and teachers have told them the truth and that the other should be persuaded by the 
+obviousness of their position. Their explanations are intentionally vague and neither appear moved.
+
+The tea pot appears to be bottomless. The conversation never raises beyond a murmur however it was clear to all passers
+by that the gravity of their purpose and the zeal they 
+
+There was no guarantee that their claims were true, nor that they were false. The great salesmen were at stalemate until
+both were forgotten and the markets have moved on, the people retired. Still they would debate.""")
+
+
+
+# The first meeting of a boyfriend to the girlfriends parents in a crowded cafe
+print("The first meeting. Titans sit as kings in front of pastries, attempting pleasentries with the timid pest who "
+      "has attempted to run away with their flock. A valuable investment frittered away by a lone actor, they are not"
+      "amuzed."
+      "'I climbed the mountain yesterday' he meekly offered, the toweringn figures barely grimacd; they walk over the "
+      "mountain to get to the shower..."
+      "Common ground was found when both parties agreed that a particular cafinated beverage was acceptable in time of "
+      "economic distress and unreasonable taxes."
+      "A bonding activity was decided and that afternoon everyone in attendance decimated the kingdom that was impressing"
+      "their taxation."
+      "Everyone, but certainly not anyone in the kingdom, lived mediocerly ever after.")
+
