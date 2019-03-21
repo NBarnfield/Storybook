ee7b6ef9118dfccc0369885764d39c0942a93726
NBarnfield <22405972+NBarnfield@users.noreply.github.com>
2019-03-21 13:45:56 +1100

    New situations for insertion into stories.

--- Situations/nexus.py
+++ Situations/nexus.py
@@ -23,3 +23,64 @@ print("The first meeting. Titans sit as kings in front of pastries, attempting p
       "their taxation."
       "Everyone, but certainly not anyone in the kingdom, lived mediocerly ever after.")
 
+
+# A kiss goodnight
+
+print("A boy sits on the side of a bed, elated and in love. Not for the boy in the bed with whom he made love, but"
+      "for the man that he loves with all his heart."
+      "It is possible to enjoy the pleasures of company without your mate. Time to yourself is a blessing."
+      "Enjoying the growth of another, supporting their strengths and suplimenting their weaknesses with praise is"
+      "a joy most profound."
+      "Turmoil and the lose of purpose - the early retirement from a job is devestating. Apathy leading up to the"
+      "dismissal, gladness; passion, a crushing tale."
+      "Sharing a moment of growth compels humanity, garners respect."
+      "Tired and alone, the boy sleeps knowing that tomorrow is a gift once thought forgone.")
+
+
+# The Nexus
+print("A center of a multi-verse, the nexus is a meeting place of trading ideas and chattels."
+      "Religion is exchanged between willing buyers, profits sought between interested parties offering that months"
+      "best returns on the after life."
+      "Goods are sold and traded in the hope that long term gain is for the lazy."
+      "Yet, it is the coffee brought over from a distant world in a world of 3 dimensions covered in liquid most "
+      "strange that facilitates the conversations of all peoples. All people have the same stature when seated,"
+      "most visual signals of status are hidden."
+      "Upon the many tables of the Nexus lies this equaliser that promotes debate, laguhter, and love."
+      "Here we trade our coin for the chance and opportunity of Hope."
+)
+
+
+# Bipolar
+print("Where do the drugs come from that prevent me from throwing myself in front of a bus?"
+      "Years passed with dozens of meetings, practicioners and supportive friends. All relationships failed except"
+      "the one with my psychatrist who enjoys my coin too much to leave me."
+      "I have made new friends and stirred new love. I love each day and live with hope, leveled out b my dugs and"
+      "kept stable by my will."
+      "Quiet and artistic expression is what I need, and that starts with a focused mind and a clear soul.")
+
+# Bodyline
+print("If it's not in a sling, it doesn't get a swing.")
+
+# The fall of a leader
+print("Witnessing the fall of an honest, hard working and faithful minster who fought for principles and genuine public "
+      "interest has been a pivotal moment for me. A life in politics is no longer a possible career. To work for a"
+      "decade and have your career ripped from under your feet is a  fate I dare not entertain."
+      "Throwing away a party member for self interested causes thereby depriving the people of a capable leader is"
+      "a foreshadowing of what a future might look like were I to enter the fray."
+      "I will have a public life, but it will be one of advocacy and entrepreneurship."
+      "I will not be torn down by the likes of self serving regressionists and zealots.")
+
+
+
+# Barbarra
+print("Trundeling down the road with a bouqet of yellow and red roses Barbarra believes she whispers as the street echos"
+      "with the words 'Service is lowzy. Always was.")
+
+# Waiter
+print("Where ettiquette would dictate that the acknoledgement of a sentient being would be polite, if not encouraged,"
+      "the waiter sees only their objective in front of them. THey have no time for your bullshit, and certainly no"
+      "patience for your interpretation of hospitality."
+      "No opened door or well meaning eye contact will steer them from their goal, and you are a fool if you think"
+      "otherwise.")
+
+
